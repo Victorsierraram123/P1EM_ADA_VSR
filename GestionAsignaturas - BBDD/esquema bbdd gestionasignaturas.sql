3f797ee40fbb9569c2f2aa57491943650d95efee
unknown <victor.sierra.ramiro@alumnos.upm.es>
2019-04-04 16:22:59 +0200

    Modificacion bbdd

--- GestionAsignaturas - BBDD/esquema bbdd gestionasignaturas.sql
+++ GestionAsignaturas - BBDD/esquema bbdd gestionasignaturas.sql
@@ -266,3 +266,12 @@ ENGINE = InnoDB;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+
+INSERT INTO `gestionasignaturas`.`convocatoria` (`idCONVOCATORIA`, `actual`) VALUES ('ordinaria', b'1');
+INSERT INTO `gestionasignaturas`.`curso` (`Cod_CURSO`, `actual`) VALUES ('2016', b'1');
+INSERT INTO `gestionasignaturas`.`examen` (`Cod_Ex`) VALUES ('ordinaria2016');
+INSERT INTO `gestionasignaturas`.`grupo_clase` (`Cod_GC`) VALUES ('1');
+INSERT INTO `gestionasignaturas`.`grupo_clase` (`Cod_GC`) VALUES ('2');
+INSERT INTO `gestionasignaturas`.`practica` (`Cod_Practica`) VALUES ('ordinaria2016');
+INSERT INTO `gestionasignaturas`.`tipo_evaluacion` (`Ev_Continua`, `Peso_Ex`, `Peso_Pr`, `Peso_Ev_C`) VALUES ('1', '50', '25', '25');
